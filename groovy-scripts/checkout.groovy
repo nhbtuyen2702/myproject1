2d337a9327988a90fe4ba5a4db697d23f038e9f4
TuyenNguyen <nhbtuyen2702@gmail.com>
2024-10-04 21:06:43 +0700

    c1

--- groovy-scripts/checkout.groovy
+++ groovy-scripts/checkout.groovy
@@ -2,7 +2,6 @@ def checkout() {
     echo "Checking out code from repository..."
     checkout scm
     echo "Current working directory: ${pwd()}"
-    sh 'ls -al'
     echo "Code checked out successfully."
 }
 
